301b820ad5c87173191edee524ec3c9c9d35f888
gusleak <57176171+gusleak@users.noreply.github.com>
2020-06-09 18:41:18 +0200

    Add files via upload

---
+++ Exercise1-Stopwatch/Stopwatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise1_Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime;
+        public TimeSpan Duration;
+
+        public DateTime Start()
+        {
+            StartTime = DateTime.Now;
+            return StartTime;
+        }
+
+        public TimeSpan Stop()
+
+        {
+            var stopTime = DateTime.Now;
+            var Duration = stopTime - StartTime;
+            return Duration;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
