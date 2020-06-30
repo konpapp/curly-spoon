771e7fd4d37b78fbb34f1110ac2e42bf80532ce2
Konstantinos Pappas <57176171+gusleak@users.noreply.github.com>
2020-06-30 20:38:05 +0200

    Create pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //Get height
+    int height;
+    do
+    {
+        height  = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    //printf("Stored: %i\n", height);
+
+    for (int i = 0; i < height; i++)
+    {
+        //moving towards right
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        //building pyramid
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
