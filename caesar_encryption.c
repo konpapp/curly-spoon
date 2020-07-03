7d812c34c22acf863cb232aa63c8298d7ec3425f
Konstantinos Pappas <57176171+gusleak@users.noreply.github.com>
2020-07-03 21:33:45 +0200

    Create caesar_encryption.c

---
+++ caesar_encryption.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    //check for integer key
+    if (argc != 2 || atoi(argv[1]) == 0)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    //check fif all digits integers
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    //rotation number
+    int key = atoi(argv[1]);
+
+    string plaintext = get_string("plaintext: ");
+    int n = strlen(plaintext);
+
+    char ciphertext[n];
+    printf("ciphertext: ");
+
+    //minimize key value inside alphabetical range
+    while (key > 26)
+    {
+        key = key % 26;
+    }
+
+    int buffer;
+    for (int i = 0 ; i < n; i++)
+    {
+        buffer = plaintext[i] + key;
+        //if character is lowercase, wrap a-z
+        if (islower(plaintext[i]))
+        {
+            while (buffer > 'z')
+            {
+                buffer -= 26;
+            }
+            ciphertext[i] = buffer;
+        }
+        //if character is uppercase, wrap A-Z
+        else if (isupper(plaintext[i]))
+        {
+            while (buffer > 'Z')
+            {
+                buffer -= 26;
+            }
+            ciphertext[i] = buffer;
+        }
+        //else just assign character
+        else
+        {
+            ciphertext[i] = plaintext[i];
+        }
+
+        printf("%c", ciphertext[i]);
+    }
+    printf("\n");
+    return 0;
+}
